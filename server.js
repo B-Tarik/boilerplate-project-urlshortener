8e7c13cca4e3e0436b26f621f0a26209180210c6
B-Tarik <tarik.bouchekkif@gmail.com>
2019-03-13 23:18:11 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
 'use strict';
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+const express = require('express');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
-var cors = require('cors');
+const setUrl = require('./handlers/setUrl');
+const getUrl = require('./handlers/getUrl');
+const errorHandler = require('./handlers/error');
 
-var app = express();
+const cors = require('cors');
+
+const app = express();
 
 // Basic Configuration 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true
+}, function(err){
+  if(err){
+    console.log(err);
+  }else {
+    console.log("Conected to DataBase.");
+  }
+});
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
@@ -31,6 +45,17 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/shorturl/:urlId", getUrl);
+
+app.post("/api/shorturl/new", setUrl);
+
+
+app.use(function(req, res, next) {
+  const err = new Error('Not Found')
+  next(err);
+})
+
+app.use(errorHandler);
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
